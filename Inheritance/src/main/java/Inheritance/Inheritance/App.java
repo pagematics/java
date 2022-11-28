931a8367ab5612ccd101ea25ab35371247b88202
alka-john94 <alka.john@pagematics.in>
2022-11-28 12:53:33 +0530

    Alka: committing inheritance

---
+++ Inheritance/src/main/java/Inheritance/Inheritance/App.java
@@ -0,0 +1,13 @@
+package Inheritance.Inheritance;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
